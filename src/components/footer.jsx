1da79e3baf5f7ef74b0ce1ef08b791a6cf45ab9c
Pablo de Rivas <pabloderivas@outlook.com>
2025-04-11 14:34:34 -0300

    Hacer equipo y footer responsive para telefono

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,12 +5,12 @@ import youtube from '../assets/youtube.svg'
 
 function Footer(){
     return(
-        <footer className='flex flex-row justify-end items-center grande:mr-10 mr-7'>
-            <div className='grande:mr-10 mr-8 lato'>
+        <footer className='flex sm:flex-row flex-col justify-end items-center grande:mr-10 sm:mr-7'>
+            <div className='grande:mr-10 mr-8 lato sm:text-left text-center'>
                 <p className='text-textogris font-medium grande:text-2xl text-xl'>Río Cuarto, Córdoba.</p>
                 <p className='text-textogris font-bold grande:text-2xl text-xl latobolditalic'>+54 9 358 501 1703</p>
                 <p className='text-textogris font-medium grande:text-2xl text-xl'>fundacionplenavi@gmail.com</p>
-                <div className='mt-3 flex items-center'>
+                <div className='sm:mt-3 flex items-center'>
                     <img className='grande:w-12 w-8 mx-1' src={facebook} alt="" />
                     <img className='grande:w-12 w-8 mx-1' src={instagram} alt="" />
                     <img className='grande:w-12 w-8 mx-1' src={youtube} alt="" />
